8e980dcc02a2eee5368ceb389e9ec359bf0a41c4
UrielMaD <urielmd00@gmail.com>
2021-02-18 23:39:53 -0600

    Carousel and images with hash added

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,10 @@ module.exports = {
         test: /\.(png|jpg)$/, 
         use: [
           {
-            loader: 'url-loader?limit=8192',
+            'loader': 'file-loader',
+            options: {
+              name: 'assets/[hash].[ext]'
+            }
           }
         ] 
       },
